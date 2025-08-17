f00d8bf55fd45c33913768bf0484f3d44f831a1e
Bobshop verbytski <nikolasdelunko@gmail.com>
2025-08-17 15:42:09 +0200

    add fetch and del func

--- back_end/index.js
+++ back_end/index.js
@@ -1,13 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // const notepad = require("./routers/notepad-rout")
 import notepad from "./routers/notepad-rout.js";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use("/api", notepad);
 
